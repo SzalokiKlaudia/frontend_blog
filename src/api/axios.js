fda48d0e1f8f85eb61f70515eefc554ec8012b88
SzalokiKlaudia <szaloki.klaudia90@gmail.com>
2026-02-07 20:41:40 +0100

    ini React

---
+++ src/api/axios.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+
+export const myAxios = axios.create({
+  baseURL: "http://localhost:8000/",
+
+  withCredentials: true, //kérések atzonosítása cookie-k segítségével
+  withXSRFToken: true,
+});
+
+myAxios.interceptors.request.use(
+  (config) => {
+    // CSRF token lekérése a cookie-ból
+    const token = document.cookie
+      .split("; ")
+      .find((row) => row.startsWith("XSRF-TOKEN="))
+      ?.split("=")[1];
+
+    // Ha találunk tokent, hozzáadjuk a headerhez
+    if (token) {
+      config.headers["X-XSRF-TOKEN"] = decodeURIComponent(token);
+    } else {
+      console.warn("No CSRF token found in cookies.");
+    }
+    // A config visszaadása, hogy a kérés folytatódhasson
+    return config;
+  },
+
+  (error) => {
+    // Hiba esetén írjuk ki a hibát, vagy végezzünk hibakezelést
+    console.error("Request interceptor error:", error);
+    return Promise.reject(error);
+  }
+);
